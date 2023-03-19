45419d92bb0c757bb7dffba2d584e4044d95e47a
Michał Rudzki <michal@mailx.com.pl>
2023-03-19 17:46:40 +0100

    re-arange the files under the repo

--- csv_changer.py
+++ csv_changer.py
@@ -1,35 +1,5 @@
-import csv
 import sys
-from modules import FileExport
-
-class ChangeFile(FileExport):
-
-    def __init__(self, *args):
-        self.input_file = args[0]
-        self.output_file = args[1]
-    
-    def load_file(self):
-        content_file = []
-        with open(self.input_file, mode = 'r', newline = '') as csvfile:
-            file = csv.reader(csvfile, delimiter = ' ')
-            for read_file in file:
-                content_file.append(read_file)
-        return content_file
-    
-    def set_col_row_change(self, cli_args, csv_in_file):
-        tmp = []
-        output_csv = []
-        for count in range(len(cli_args)):
-            row = int(cli_args[count].split(',')[0])
-            col = int(cli_args[count].split(',')[1])
-            val = cli_args[count].split(',')[2]
-            tmp.append(csv_in_file[int(col)][0])
-            if count == col:
-                t = tmp[col].split(',')
-                t[row] = val
-                output_csv.append(t)
-        return output_csv
-                
+from package.changefile import ChangeFile
 #     0          1          2             3           4         5 
 # ["in.csv", "out.csv", "0,0,gitara", "3,1,kubek", "1,2,17", "3,3,0"]
 def main():
