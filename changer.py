87a3791784186bbd96432a17428f410ce4a67d19
Michał Rudzki <michal@mailx.com.pl>
2023-03-22 22:53:01 +0100

    template for exporting files to specific format, independent from base class

--- changer.py
+++ changer.py
@@ -1,15 +1,21 @@
 import sys
-from package.changefile import ChangeFile
+
+from package.csv_export import CSV_export
+from package.json_export import JSON_export
 #     0          1          2             3           4         5 
 # ["in.csv", "out.csv", "0,0,gitara", "3,1,kubek", "1,2,17", "3,3,0"]
 
 def main():
     # "in.csv", "out.pickle", "0,0,gitara", "3,1,kubek", "1,2,17", "3,3,0"
-    file = ChangeFile(sys.argv[1], sys.argv[2])
-    csv_content = file.load_file()
-    csv_output_content = file.set_col_row_change(sys.argv[3:], csv_content)
-    file.save_file(sys.argv[2], csv_output_content)
-
+    if sys.argv[1].split('.')[1] in ['csv']:
+        print(f"Input file: {sys.argv[1]}")
+        file = CSV_export(sys.argv[1], sys.argv[2])
+        file.export_convert()
+    
+    elif sys.argv[1].split('.')[1] in ['json']:
+        print(f"Input file: {sys.argv[1]}")
+        file = JSON_export(sys.argv[1], sys.argv[2])
+        file.export_convert()
 
 if __name__ == "__main__":
     main()
