874011946eb8e81f40370ebc25aadd3cef34eaf2
Michał Rudzki <michal@mailx.com.pl>
2023-03-24 22:05:57 +0100

    configured txt file support

--- changer.py
+++ changer.py
@@ -2,6 +2,7 @@ import sys
 
 from package.csv_export import CSV_export
 from package.json_export import JSON_export
+from package.txt_export import TXT_export
 #     0          1          2             3           4         5 
 # ["in.csv", "out.csv", "0,0,gitara", "3,1,kubek", "1,2,17", "3,3,0"]
 
@@ -16,6 +17,10 @@ def main():
         print(f"Input file: {sys.argv[1]}")
         file = JSON_export(sys.argv[1], sys.argv[2])
         file.export_convert()
+
+    elif sys.argv[1].split('.')[1] in ['txt']:
+        print(f"Input file: {sys.argv[1]}")
+        file = []
 
 if __name__ == "__main__":
     main()
