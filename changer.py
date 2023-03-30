e180cb576b6957d210219d544dba67f475152ffa
Michał Rudzki <33937084+michal-rudzki@users.noreply.github.com>
2023-03-30 15:00:28 +0200

    Gh 02 (#6)
    * created seperate files for each extension
    
    * small update under csv_changer.py
    
    * gitignore file
    
    * updated ignore file, removed pycache
    
    * template for exporting files to specific format, independent from base class
    
    * template for exporting files to specific format, independent from base class
    
    * fixing the csv imports
    
    * implementing JSON read, convert, save functionality atound the base class
    
    * configured txt file support
    
    * reconfigure ChangeFile_export to passing in/out file name and data
    
    * JSON_export now supports Change_export_convert
    
    * Configure normalization output_data for JSON/CSV and exporting to the JSON/CSV
    
    * Configure import for PICKLE
    
    * TXT support converter
    
    * normalization for output_data under ChangeFile
    
    * reconfigure import for ChangeFile
    
    * add PICKLE functionality
    
    * minot change under the ChangeFile()
    
    * implement PICKLE finctionality
    
    * example input files
    
    * removing files
    
    * del file
    
    * edit exception files
    
    * minor change for PICKLE support under the main file
    
    * reconfigure object to suport arguments list from sys.argv
    
    * reconfigure __init__ to support data from *args instead of global sys.argv

--- changer.py
+++ changer.py
@@ -11,22 +11,22 @@ def main():
     # "in.csv", "out.pickle", "0,0,gitara", "3,1,kubek", "1,2,17", "3,3,0"
     if sys.argv[1].split('.')[1] in ['csv']:
         print(f"Input file: {sys.argv[1]}")
-        file = CSV_export(sys.argv[1], sys.argv[2])
+        file = CSV_export(sys.argv[1], sys.argv[2], sys.argv[3:])
         file.export_convert()
     
     elif sys.argv[1].split('.')[1] in ['json']:
         print(f"Input file: {sys.argv[1]}")
-        file = JSON_export(sys.argv[1], sys.argv[2])
+        file = JSON_export(sys.argv[1], sys.argv[2], sys.argv[3:])
         file.export_convert()
     
     elif sys.argv[1].split('.')[1] in ['txt']:
         print(f"Input file: {sys.argv[1]}")
-        file = TXT_export(sys.argv[1], sys.argv[2])
+        file = TXT_export(sys.argv[1], sys.argv[2], sys.argv[3:])
         file.export_convert()
     
     elif sys.argv[1].split('.')[1] in ['pickle']:
         print(f"Input file: {sys.argv[1]}")
-        file = PICKLE_export(sys.argv[1], sys.argv[2])
+        file = PICKLE_export(sys.argv[1], sys.argv[2], sys.argv[3:])
         file.export_convert()
 
 if __name__ == "__main__":
--- package/csv_export.py
+++ package/csv_export.py
@@ -5,13 +5,14 @@ from .changefile import ChangeFile
 class CSV_export(ChangeFile):
     
     def __init__(self, *args):
-        self.input_file = sys.argv[1]
-        self.output_file = sys.argv[2]
+        self.input_file = args[0]
+        self.output_file = args[1]
+        self.arguments = args[2]
     
     def export_convert(self):
         _read_file = self.CSV_export_read_file()
         _converted_data = self.CSV_export_change_row_col(_read_file)
-        if sys.argv[2].split('.')[1] in ['csv']:
+        if self.output_file.split('.')[1] in ['csv']:
             self.CSV_export_save_file(_converted_data)
         else:
             self.ChangeFile_export_convert(_converted_data, self.output_file, self.input_file)
@@ -25,9 +26,9 @@ class CSV_export(ChangeFile):
         return content_file
     
     def CSV_export_change_row_col(self, file_content):
-        print("Output file: ", sys.argv[2])
-        print("Arguments: ", sys.argv[3:])
-        cli_args = sys.argv[3:]
+        print("Output file: ", self.output_file)
+        print("Arguments: ", self.arguments)
+        cli_args = self.arguments
         tmp = []
         output_csv = []
         for count in range(len(cli_args)):
@@ -42,6 +43,6 @@ class CSV_export(ChangeFile):
         return output_csv
     
     def CSV_export_save_file(self, data_to_save):
-        with open(sys.argv[2], mode = 'w', newline = '') as csv_file:
+        with open(self.output_file, mode = 'w', newline = '') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerows(data_to_save)
